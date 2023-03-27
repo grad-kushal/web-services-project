3868115824917d222123818901c33e01bb5bbca0
grad-kushal <ksk7657@g.rit.edu>
2023-03-27 13:46:28 -0400

    Add rmse function

--- matrix_factorization.py
+++ matrix_factorization.py
@@ -12,13 +12,31 @@ def matrix_factorization_based_collaborative_filtering(matrix):
     """
     # Convert the matrix into a numpy array
     np_matrix = np.array(matrix)
-    number_of_components = 5
+    number_of_components = 15
 
     # Perform matrix factorization
     model = NMF(n_components=number_of_components, init='random', random_state=0)
     W = model.fit_transform(np_matrix)
     H = model.components_
     return W, H
+
+
+def calculate_root_mean_square_error(matrix, predictions):
+    """
+    Calculate the root mean square error
+    :param matrix: Matrix
+    :param predictions: Predictions
+    :return: Root mean square error
+    """
+    # Convert the matrix into a numpy array
+    np_matrix = np.array(matrix)
+
+    # Calculate the root mean square error
+    error = np_matrix - predictions
+    square_error = error ** 2
+    mean_square_error = square_error.mean()
+    root_mean_square_error = np.sqrt(mean_square_error)
+    return root_mean_square_error
 
 
 def main():
@@ -28,7 +46,8 @@ def main():
     matrix = parser.create_matrix(mashup_records, api_records)
     mashup_matrix, api_matrix = matrix_factorization_based_collaborative_filtering(matrix)
     predictions = np.dot(mashup_matrix, api_matrix)
-    print(predictions[0])
+    root_mean_square_error = calculate_root_mean_square_error(matrix, predictions)
+    print('Root mean square error: ' + str(root_mean_square_error))
 
 
 if __name__ == "__main__":
